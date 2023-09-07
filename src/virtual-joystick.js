266ad9652ef8f239118bdcdd554546fb2357a870
Santosh Maurya <83776025+Joker-pyc@users.noreply.github.com>
2023-09-08 00:17:56 +0530

    Update virtual-joystick.js
    both touch and mouse event supported

--- src/virtual-joystick.js
+++ src/virtual-joystick.js
@@ -6,140 +6,160 @@ licensed under:
 MIT license
 */
 
-    class VirtualJoystick {
-  constructor(container, options = {}) {
-    this.container = container;
-    this.options = Object.assign(
-      {
-        width: 100,
-        height: 100,
-        color: 'gray',
-        handleColor: 'white',
-        handleRadius: 20,
-        onChange: null,
-      },
-      options
-    );
 
-    this.joystick = document.createElement('div');
-    this.joystick.style.width = `${this.options.width}px`;
-    this.joystick.style.height = `${this.options.height}px`;
-    this.joystick.style.borderRadius = '50%';
-    this.joystick.style.backgroundColor = this.options.color;
-    this.joystick.style.position = 'relative';
-    this.joystick.style.touchAction = 'none';
-    this.joystick.style.border = '3px solid black';
-    this.joystick.style.opacity = '50%';
-    this.container.appendChild(this.joystick);
+  class VirtualJoystick {
+            constructor(container, options = {}) {
+                this.container = container;
+                this.options = Object.assign(
+                    {
+                        width: 100,
+                        height: 100,
+                        color: 'gray',
+                        handleColor: 'white',
+                        handleRadius: 20,
+                        onChange: null,
+                    },
+                    options
+                );
 
-    this.handle = document.createElement('div');
-    this.handle.style.width = `${this.options.handleRadius * 2}px`;
-    this.handle.style.height = `${this.options.handleRadius * 2}px`;
-    this.handle.style.borderRadius = '50%';
-    this.handle.style.backgroundColor = this.options.handleColor;
-    this.handle.style.position = 'absolute';
-    this.handle.style.top = `${this.options.height / 2 - this.options.handleRadius}px`;
-    this.handle.style.left = `${this.options.width / 2 - this.options.handleRadius}px`;
-    this.handle.style.touchAction = 'none';
-    this.joystick.appendChild(this.handle);
+                this.joystick = document.createElement('div');
+                this.joystick.style.width = `${this.options.width}px`;
+                this.joystick.style.height = `${this.options.height}px`;
+                this.joystick.style.borderRadius = '50%';
+                this.joystick.style.backgroundColor = this.options.color;
+                this.joystick.style.position = 'relative';
+                this.joystick.style.touchAction = 'none';
+                this.joystick.style.border = '3px solid black';
+                this.joystick.style.opacity = '50%';
+                this.container.appendChild(this.joystick);
 
-    this.position = { x: 0, y: 0 };
-    this.delta = { x: 0, y: 0 };
-    this.handleRadius = this.options.handleRadius;
-    this.joystickRect = this.joystick.getBoundingClientRect();
+                this.handle = document.createElement('div');
+                this.handle.style.width = `${this.options.handleRadius * 2}px`;
+                this.handle.style.height = `${this.options.handleRadius * 2}px`;
+                this.handle.style.borderRadius = '50%';
+                this.handle.style.backgroundColor = this.options.handleColor;
+                this.handle.style.position = 'absolute';
+                this.handle.style.top = `${this.options.height / 2 - this.options.handleRadius}px`;
+                this.handle.style.left = `${this.options.width / 2 - this.options.handleRadius}px`;
+                this.handle.style.touchAction = 'none';
+                this.joystick.appendChild(this.handle);
 
-    this.isPressed = false;
-    this.touchId = null;
+                this.position = { x: 0, y: 0 };
+                this.delta = { x: 0, y: 0 };
+                this.handleRadius = this.options.handleRadius;
+                this.joystickRect = this.joystick.getBoundingClientRect();
 
-    this._addEventListeners();
-  }
+                this.isPressed = false;
+                this.touchId = null;
 
-  _addEventListeners() {
-    this.handle.addEventListener('mousedown', this._handleMouseDown.bind(this));
-    document.addEventListener('mousemove', this._handleMouseMove.bind(this));
-    document.addEventListener('mouseup', this._handleMouseUp.bind(this));
-    this.handle.addEventListener('touchstart', this._handleTouchStart.bind(this));
-    document.addEventListener('touchend', this._handleTouchEnd.bind(this));
-    document.addEventListener('touchcancel', this._handleTouchEnd.bind(this));
-    document.addEventListener('touchmove', this._handleTouchMove.bind(this));
-  }
+                this._addEventListeners();
+            }
 
-  _handleMouseDown(event) {
-    this.isPressed = true;
-    this._updatePosition(event.clientX, event.clientY);
-  }
+            _addEventListeners() {
+                this.handle.addEventListener('mousedown', this._handleMouseDown.bind(this));
+                document.addEventListener('mousemove', this._handleMouseMove.bind(this));
+                document.addEventListener('mouseup', this._handleMouseUp.bind(this));
+                this.handle.addEventListener('touchstart', this._handleTouchStart.bind(this));
+                document.addEventListener('touchend', this._handleTouchEnd.bind(this));
+                document.addEventListener('touchcancel', this._handleTouchEnd.bind(this));
+                document.addEventListener('touchmove', this._handleTouchMove.bind(this));
+            }
 
-  _handleMouseMove(event) {
-    if (this.isPressed) {
-      this._updatePosition(event.clientX, event.clientY);
-    }
-  }
+            _handleMouseDown(event) {
+                this.isPressed = true;
+                this._updatePosition(event.clientX, event.clientY);
+            }
 
-  _handleMouseUp() {
-    this.isPressed = false;
-    this._resetPosition();
-  }
+            _handleMouseMove(event) {
+                if (this.isPressed) {
+                    this._updatePosition(event.clientX, event.clientY);
+                }
+            }
 
-  _handleTouchStart(event) {
-    if (this.touchId === null) {
-      this.touchId = event.changedTouches[0].identifier;
-      this.isPressed = true;
-      this._updatePosition(event.changedTouches[0].clientX, event.changedTouches[0].clientY);
-    }
-  }
+            _handleMouseUp() {
+                this.isPressed = false;
+                this._resetPosition();
+            }
 
-  _handleTouchEnd(event) {
-    for (let i = 0; i < event.changedTouches.length; i++) {
-      if (event.changedTouches[i].identifier === this.touchId) {
-        this.touchId = null;
-        this.isPressed = false;
-        this._resetPosition();
-        break;
-      }
-    }
-  }
+            _handleTouchStart(event) {
+                if (this.touchId === null) {
+                    this.touchId = event.changedTouches[0].identifier;
+                    this.isPressed = true;
+                    this._updatePosition(event.changedTouches[0].clientX, event.changedTouches[0].clientY);
+                }
+            }
 
-  _handleTouchMove(event) {
-    for (let i = 0; i < event.changedTouches.length; i++) {
-      if (event.changedTouches[i].identifier === this.touchId) {
-        this._updatePosition(event.changedTouches[i].clientX, event.changedTouches[i].clientY);
-        break;
-      }
-    }
-  }
+            _handleTouchEnd(event) {
+                for (let i = 0; i < event.changedTouches.length; i++) {
+                    if (event.changedTouches[i].identifier === this.touchId) {
+                        this.touchId = null;
+                        this.isPressed = false;
+                        this._resetPosition();
+                        break;
+                    }
+                }
+            }
 
-  _updatePosition(x, y) {
-    const dx = x - this.joystickRect.left - this.options.width / 2;
-    const dy = y - this.joystickRect.top - this.options.height / 2;
-    const distance = Math.sqrt(dx * dx + dy * dy);
+            _handleTouchMove(event) {
+                for (let i = 0; i < event.changedTouches.length; i++) {
+                    if (event.changedTouches[i].identifier === this.touchId) {
+                        this._updatePosition(event.changedTouches[i].clientX, event.changedTouches[i].clientY);
+                        break;
+                    }
+                }
+            }
 
-    if (distance > this.handleRadius) {
-      const angle = Math.atan2(dy, dx);
-      this.position = { x: Math.cos(angle) * this.handleRadius, y: Math.sin(angle) * this.handleRadius };
-    } else {
-      this.position = { x: dx, y: dy };
-    }
+            _updatePosition(x, y) {
+                let clientX = x;
+                let clientY = y;
 
-    this.delta = { x: this.position.x / this.handleRadius, y: this.position.y / this.handleRadius };
+                if (this.touchId !== null) {
+                    for (let i = 0; i < event.changedTouches.length; i++) {
+                        if (event.changedTouches[i].identifier === this.touchId) {
+                            clientX = event.changedTouches[i].clientX;
+                            clientY = event.changedTouches[i].clientY;
+                            const dx = x - this.joystickRect.left - this.options.width / 2 + this.options.handleRadius;
+                            const dy = y - this.joystickRect.top - this.options.height / 2 + this.options.handleRadius;
 
-    this.handle.style.transform = `translate(${this.position.x}px, ${this.position.y}px)`;
+                            break;
+                        }
+                    }
+                }
 
-    if (typeof this.options.onChange === 'function') {
-      this.options.onChange(this.delta);
-    }
-  }
+                const dx = clientX - this.joystickRect.left - this.options.width / 2;
+                const dy = clientY - this.joystickRect.top - this.options.height / 2;
 
-  _resetPosition() {
-    this.position = { x: 0, y: 0 };
-    this.delta = { x: 0, y: 0 };
+                const distance = Math.sqrt(dx * dx + dy * dy);
 
-    this.handle.style.transform = `translate(0, 0)`;
+                if (distance > this.handleRadius) {
+                    const angle = Math.atan2(dy, dx);
+                    this.position = { x: Math.cos(angle) * this.handleRadius, y: Math.sin(angle) * this.handleRadius };
+                } else {
+                    this.position = { x: dx, y: dy };
+                }
 
-    if (typeof this.options.onChange === 'function') {
-      this.options.onChange(this.delta);
-    }
-  }
-}
+                this.delta = { x: this.position.x / this.handleRadius, y: this.position.y / this.handleRadius };
+
+                this.handle.style.transform = `translate(${this.position.x}px, ${this.position.y}px)`;
+
+                if (typeof this.options.onChange === 'function') {
+                    this.options.onChange(this.delta);
+                }
+            }
+
+            _resetPosition() {
+                this.position = { x: 0, y: 0 };
+                this.delta = { x: 0, y: 0 };
+
+                this.handle.style.transform = `translate(0, 0)`;
+
+                if (typeof this.options.onChange === 'function') {
+                    this.options.onChange(this.delta);
+                }
+            }
+        }
+
+
 
 export default VirtualJoystick;
       
